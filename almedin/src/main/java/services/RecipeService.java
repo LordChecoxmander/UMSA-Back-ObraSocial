4231312ea5728a50aefe3a60b376f6ff3695ae55
MadelaineDiLauro22 <madeludmila@outlook.com>
2024-06-08 20:30:21 -0300

    Cambios y creacion
    Se genero el codigo correspondiente para la funcionalidad de RecipeService y se creo el package Utils junto con la clase PDFGenerator

--- almedin/src/main/java/services/RecipeService.java
+++ almedin/src/main/java/services/RecipeService.java
@@ -1,22 +1,30 @@
-package services;
+package resources;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 import modelsEntities.Recipe;
-import repositories.RecipeRepository;
+import services.RecipeService;
+import utils.PdfGenerator;
 
-@ApplicationScoped
-public class RecipeService {
+@Path("/recipes")
+public class RecipeResource {
 
     @Inject
-    RecipeRepository recipeRepository;
+    private RecipeService recipeService;
 
-    public Recipe obtenerRecetaPorTurno(Long turnoId) {
-        Recipe receta = recipeRepository.find("shift.id", turnoId).firstResult();
-        if (receta == null || !receta.isActive()) {
-            throw new RuntimeException("Receta no válida o no encontrada");
-        }
-        return receta;
+    @GET
+    @Path("{id}")
+    @Produces("application/pdf")
+    public Response downloadRecipe(@PathParam("id") Long shiftId) {
+        Recipe recipe = recipeService.getRecipeByShiftId(shiftId);
+        byte[] pdfData = PdfGenerator.generatePdf(recipe);
+
+        return Response.ok(pdfData)
+                .header("Content-Disposition", "attachment; filename=recipe_" + shiftId + ".pdf")
+                .build();
     }
 }
